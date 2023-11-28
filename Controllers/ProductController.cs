2a7fcc904593f0b9dda5aa08963c77fa032a2b72
MatiasKauranen <94970023+MatiasKauranen@users.noreply.github.com>
2023-11-28 02:22:19 +0200

    Review feature update

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using online_store_app.Data;
 using online_store_app.Models;
+using System;
 using System.Linq;
 
 namespace online_store_app.Controllers
@@ -34,6 +35,44 @@ namespace online_store_app.Controllers
 
             return View(product);
         }
+
+
+        [HttpGet]
+        public IActionResult Review(int productId)
+        {
+            var model = new ProductReview { ProductId = productId };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Review(ProductReview model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Assuming you are using Azure SQL Authentication, replace this with your logic to get the current user ID
+                var userId = "<Your logic to get user ID>";
+
+                var review = new ProductReview
+                {
+                    ProductId = model.ProductId,
+                    Title = model.Title,
+                    Content = model.Content,
+                    Rating = model.Rating,
+                    UserId = userId,
+                    CreatedAt = DateTime.Now
+                };
+
+                _context.ProductReviews.Add(review);
+                _context.SaveChanges();
+
+                // Redirect to the main menu after submitting the review
+                return RedirectToAction("Index", "Home");
+            }
+
+            // If there are validation errors, return to the review form with the model
+            return View(model);
+        }
+
 
         // Add other actions as needed
     }
