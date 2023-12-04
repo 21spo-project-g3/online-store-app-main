4c4baec707257a5c6fe433c272353e2f8c3a8961
MatiasKauranen <94970023+MatiasKauranen@users.noreply.github.com>
2023-12-04 18:42:12 +0200

    Inventory management page and alert for low stock
    Admin now has access to inventory management page where you can update product quantity which is stored in database. Low quantity alert is shown if stock gets too low.

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,7 +1,10 @@
-﻿// Controllers/AdminController.cs
+﻿// AdminController.cs
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using online_store_app.Data;
+using online_store_app.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 public class AdminController : Controller
 {
@@ -15,9 +18,37 @@ public class AdminController : Controller
     [Authorize(Policy = "AdminOnly")]
     public IActionResult InventoryManagement()
     {
-        var lowQuantityProducts = _context.Products.Where(p => p.Quantity < 1000).ToList(); //Adjust what quantity level product is shown, currently show all products
-        return View(lowQuantityProducts);
+        var allProducts = _context.Products.ToList(); // Get all products
+
+        // Set ViewData for low quantity products count
+        ViewData["LowQuantityProductsCount"] = allProducts.Count(p => p.Quantity < 5);
+
+        return View(allProducts);
     }
 
+    [HttpPost]
+    [Authorize(Policy = "AdminOnly")]
+    public IActionResult UpdateQuantities(List<int> quantities, List<Product> products)
+    {
+        for (int i = 0; i < quantities.Count; i++)
+        {
+            var productId = products[i].Id;
+            var newQuantity = quantities[i];
 
+            // Retrieve the product from the database
+            var product = _context.Products.Find(productId);
+
+            if (product != null)
+            {
+                // Update the quantity
+                product.Quantity = newQuantity;
+            }
+        }
+
+        // Save changes to the database
+        _context.SaveChanges();
+
+        // Redirect back to the InventoryManagement action
+        return RedirectToAction(nameof(InventoryManagement));
+    }
 }
