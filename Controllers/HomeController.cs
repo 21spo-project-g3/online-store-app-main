ba007f463f2f90b711162575b09e8df8db7a0a5a
MatiasKauranen <94970023+MatiasKauranen@users.noreply.github.com>
2023-12-13 14:20:14 +0200

    Search feature ready
    Searching for products works now and leads to search page with product cards that match results.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using online_store_app.Data;
+using online_store_app.Models;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace online_store_app.Controllers
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ApplicationDbContext _dbContext;
+
+    public HomeController(ApplicationDbContext dbContext)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        _dbContext = dbContext;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Search(string query)
+    {
+        // Perform search logic
+        List<Product> searchResults = _dbContext.Products
+            .Where(p => p.Name.Contains(query))
+            .ToList();
+
+        return View("Search", searchResults);
     }
 }
